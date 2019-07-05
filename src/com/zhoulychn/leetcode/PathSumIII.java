2956b4432402a92ce0b4ffd55e27422a062f97e5
zhouly <zhoulychn@qq.com>
2019-07-05 11:18:43 +0800

    commit

--- src/com/zhoulychn/leetcode/PathSumIII.java
+++ src/com/zhoulychn/leetcode/PathSumIII.java
@@ -34,20 +34,11 @@ root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
 public class PathSumIII {
 
     public int pathSum(TreeNode root, int sum) {
-        int count = 0;
 
-        if (root == null) return count;
+        if (root == null) return 0;
 
-        //层次遍历，每个节点当作根查找
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        while (queue.size() > 0) {
-            TreeNode node = queue.poll();
-            count += search(node, sum);
-            if (node.left != null) queue.offer(node.left);
-            if (node.right != null) queue.offer(node.right);
-        }
-        return count;
+        //搜索以当前节点为根节点的路径+左子树+右子树
+        return search(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
 
     public int search(TreeNode root, int sum) {
@@ -56,16 +47,15 @@ public class PathSumIII {
         if (root == null) return 0;
 
         //当前路径可能已经符合，但是不保证向下找还可以符合，所以结果要+1
-        int temp = 0;
-        if (root.val == sum) temp++;
+        sum -= root.val;
 
         //当前是否完成+左子树查找+右子树查找
-        return temp + search(root.left, sum - root.val) + search(root.right, sum - root.val);
+        return (sum == 0 ? 1 : 0) + search(root.left, sum) + search(root.right, sum);
     }
 
     public static void main(String[] args) {
-        TreeNode tree = Tools.build(new Integer[]{1, -2, -3, 1, 3, -2, null, -1}, 0);
-        int i = new PathSumIII().pathSum(tree, -1);
+        TreeNode tree = Tools.build(new Integer[]{1, null, 2, null, 3, null, 4, null, 5}, 0);
+        int i = new PathSumIII().pathSum(tree, 3);
         System.out.println(i);
     }
 }
