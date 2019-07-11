50d6da867a3e871de15400d2cbd65c1e38b2faed
zhouly <zhoulychn@qq.com>
2019-07-11 16:51:23 +0800

    commit

---
+++ src/com/zhoulychn/leetcode/MergeTwoBinaryTrees.java
@@ -0,0 +1,43 @@
+package com.zhoulychn.leetcode;
+
+/*
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+
+示例 1:
+
+输入:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+输出:
+合并后的树:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+ */
+public class MergeTwoBinaryTrees {
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 != null && t2 != null) {
+
+            t1.val += t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            return t1;
+        }
+
+
+        return t1 == null ? t2 : t1;
+    }
+
+    public static void main(String[] args) {
+        new MergeTwoBinaryTrees().mergeTrees(Tools.build(new Integer[]{1, 3, 2, 5}, 0), Tools.build(new Integer[]{2, 1, 3, null, 4, null, 7}, 0));
+    }
+}
---
+++ src/com/zhoulychn/leetcode/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,52 @@
+package com.zhoulychn.leetcode;
+
+import sun.security.util.Length;
+
+import java.util.Arrays;
+
+/*
+
+给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+
+你找到的子数组应是最短的，请输出它的长度。
+
+示例 1:
+
+输入: [2, 6, 4, 8, 10, 9, 15]
+输出: 5
+解释: 你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+说明 :
+
+输入的数组长度范围在 [1, 10,000]。
+输入的数组可能包含重复元素 ，所以升序的意思是<=。
+
+ */
+public class ShortestUnsortedContinuousSubarray {
+
+    //排序后，对比第一个不同的，和最后一个不同的元素
+    public int findUnsortedSubarray(int[] nums) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(arr);
+        int lo = 0, hi = 0;
+        boolean l = false;
+        boolean r = false;
+
+        for (int i = 0, j = nums.length - 1; (!l || !r) && i < nums.length; i++, j--) {
+            if (l && r) break;
+            if (!l && arr[i] != nums[i]) {
+                lo = i;
+                l = true;
+            }
+            if (!r && arr[j] != nums[j]) {
+                hi = j;
+                r = true;
+            }
+        }
+        return l ? hi - lo + 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        int unsortedSubarray = new ShortestUnsortedContinuousSubarray().findUnsortedSubarray(new int[]{1, 3, 2, 4, 5});
+        System.out.println(unsortedSubarray);
+    }
+}
