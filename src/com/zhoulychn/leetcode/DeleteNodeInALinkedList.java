778077de6fbb8eca386f96cc5ce9532cbe38a6bb
zhouly <zhoulychn@qq.com>
2019-07-18 18:31:36 +0800

    commit

--- src/com/zhoulychn/leetcode/DeleteNodeInALinkedList.java
+++ src/com/zhoulychn/leetcode/DeleteNodeInALinkedList.java
@@ -19,5 +19,7 @@ import com.zhoulychn.common.list.ListNode;
 public class DeleteNodeInALinkedList {
 
     public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
