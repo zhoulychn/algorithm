4c71e972d56f13adb46464801d110e4b531ee63a
zhouly <zhoulychn@qq.com>
2019-08-14 09:16:22 +0800

    commit

--- src/com/zhoulychn/Tools.java
+++ src/com/zhoulychn/Tools.java
@@ -32,7 +32,7 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-        String s = nameSwitch("string-to-integer-atoi");
+        String s = nameSwitch("binary-tree-level-order-traversal");
         String str = "[[1,2,3,4],[5,6,7,8],[9,10,11,12]]".replace("[", "{").replace("]", "}");
         System.out.println(s);
         System.out.println(str);
---
+++ src/com/zhoulychn/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,59 @@
+package com.zhoulychn.leetcode;
+
+import com.zhoulychn.Tools;
+import com.zhoulychn.common.stack.SqQueue;
+import com.zhoulychn.common.tree.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        TreeNode p = root;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        res.add(temp);
+        while (queue.size() != 0) {
+            TreeNode node = queue.poll();
+            temp.add(node.val);
+            if (node.left != null) queue.addLast(node.left);
+            if (node.right != null) queue.addLast(node.right);
+            if (node == p && queue.size() != 0) {
+                p = queue.peekLast();
+                temp = new ArrayList<>();
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new BinaryTreeLevelOrderTraversal().levelOrder(Tools.build(new Integer[]{3, 9, 20, null, null, 15, 7}, 0));
+    }
+}
