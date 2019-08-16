42d9e2791ff384ada1ff4779312d7e713aa9ef29
zhoulychn <zhoulychn@qq.com>
2019-08-16 16:59:37 +0800

    commit

--- src/com/zhoulychn/leetcode/GenerateParentheses.java
+++ src/com/zhoulychn/leetcode/GenerateParentheses.java
@@ -16,15 +16,37 @@ package com.zhoulychn.leetcode;
 
  */
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class GenerateParentheses {
+
     public List<String> generateParenthesis(int n) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("(", 1);
-//        Arrays.sort();
-        return null;
+        List<String> res = new LinkedList<>();
+        HashMap<String, Integer> num = new HashMap<>();
+        HashMap<String, Integer> surplus = new HashMap<>();
+        res.add("(");
+        num.put("(", 1);
+        surplus.put("(", 1);
+        for (int i = 1; i < 2 * n; i++) {
+            List<String> temp = new LinkedList<>();
+            for (String str : res) {
+                if (num.get(str) < n) {
+                    temp.add(str + "(");
+                    num.put(str + "(", num.get(str) + 1);
+                    surplus.put(str + "(", surplus.get(str) + 1);
+                }
+                if (surplus.get(str) > 0) {
+                    temp.add(str + ")");
+                    num.put(str + ")", num.get(str));
+                    surplus.put(str + ")", surplus.get(str) - 1);
+                }
+            }
+            res = temp;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new GenerateParentheses().generateParenthesis(3);
     }
 }
